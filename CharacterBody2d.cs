07203a4d79b8a9e6ed68ab198806e51a0535a50a
Delusion <116333229+Tyukpei97@users.noreply.github.com>
2025-06-06 06:58:37 +0700

    start_to_connections

--- CharacterBody2d.cs
+++ CharacterBody2d.cs
@@ -1,21 +1,25 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class CharacterBody2d : CharacterBody2D
 {
-    [Export] private float moveSpeed ;
+    [Export] private float maxMoveSpeed;
     [Export] private float crouchMoveSpeed;
     [Export] private float jumpSpeed;
-    [Export] private float groundedTimer;
     [Export] private float groundedDelay;
-    
+    [Export] private float rollSpeedBoost;
+    [Export] private float rollDuration;
 
-    public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
+    private float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
     private AnimatedSprite2D _animations;
     private CollisionShape2D _collisionShape;
-
-
+    
+    private float _rollTimer;
+    private float _accelerationTime;
+    private float _totalTime;
+    private bool _isRolling;
+    private float _groundedTimer;
 
     public override void _Ready()
     {
@@ -23,94 +27,84 @@ public partial class CharacterBody2d : CharacterBody2D
         _collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
     }
 
-
-    public override void _PhysicsProcess(double delta) 
+    public override void _PhysicsProcess(double delta)
     {
-        
         Vector2 velocity = Velocity;
+
+        float _currentSpeed = (_accelerationTime / _totalTime) * maxMoveSpeed;
 
         if (!IsOnFloor())
         {
             velocity.Y += gravity * (float)delta;
-            groundedTimer = 0f;
+            _groundedTimer = 0.0f;
         }
         else
-            groundedTimer += (float)delta;
+            _groundedTimer += (float)delta;
 
-        velocity.X = 0;
+        velocity.X = 0.0f;
+        bool crouching = Input.IsKeyPressed(Key.Ctrl);
+        bool rollInput = Input.IsActionJustPressed("Roll") && !_isRolling && IsOnFloor() && !crouching;
 
-        if (Input.IsKeyPressed(Key.A))
+        if (_isRolling)
         {
-            velocity.X = -moveSpeed;
-            if (Input.IsKeyPressed(Key.Ctrl) && IsOnFloor())
-                velocity.X = -crouchMoveSpeed;
+            _rollTimer -= (float)delta;
+            if (_rollTimer <= 0.0f)
+                _isRolling = false;
+            velocity.X = (_animations.FlipH ? -1.0f : 1.0f) * rollSpeedBoost;
         }
-        else if (Input.IsKeyPressed(Key.D))
+        else
         {
-            velocity.X = moveSpeed;
-            if (Input.IsKeyPressed(Key.Ctrl) && IsOnFloor())
-                velocity.X = crouchMoveSpeed;
+            if (Input.IsKeyPressed(Key.A))
+            {
+                _accelerationTime += (float)delta;
+                velocity.X = crouching ? -crouchMoveSpeed : -_currentSpeed;
+            }
+            else if (Input.IsKeyPressed(Key.D))
+            {
+                _accelerationTime += (float)delta;
+                velocity.X = crouching ? crouchMoveSpeed : _currentSpeed;
+            }
         }
-        if (Input.IsKeyPressed(Key.Space) && IsOnFloor() && groundedTimer >= groundedDelay && !Input.IsKeyPressed(Key.Ctrl))
+
+        if (Input.IsKeyPressed(Key.Space) && IsOnFloor() && _groundedTimer >= groundedDelay && !crouching && !_isRolling)
         {
             velocity.Y = -jumpSpeed;
-            groundedTimer = 0f;
+            _groundedTimer = 0.0f;
         }
-        _UpdateSpriteRenderer(velocity.X, velocity.Y);
+
+        if (rollInput)
+        {
+            _isRolling = true;
+            _rollTimer = rollDuration;
+        }
+
+        UpdateSpriteRenderer(velocity.X, velocity.Y);
         Velocity = velocity;
         MoveAndSlide();
     }
 
-    private void _UpdateSpriteRenderer(float velX, float velY)
+    private void UpdateSpriteRenderer(float velX, float velY)
     {
         bool walking = Math.Abs(velX) > 0.1f;
         bool jumping = velY < -0.1f;
         bool falling = velY > 0.1f;
         bool crouching = Input.IsKeyPressed(Key.Ctrl);
-        bool rolling = Input.IsKeyPressed(Key.Shift);
 
-       var shape = (CapsuleShape2D)_collisionShape.Shape;
-        if (crouching || rolling)
-        {
-            shape.Height = 25; 
-            _collisionShape.Position = new Vector2(_collisionShape.Position.X, -15.5f);
-        }
-        else
-        {
-            shape.Height = 35;
-            _collisionShape.Position = new Vector2(_collisionShape.Position.X, -20); 
-        }
-        
+        var shape = (CapsuleShape2D)_collisionShape.Shape;
+        bool isLowProfile = crouching || _isRolling;
+        shape.Height = isLowProfile ? 25.0f : 35.0f;
+        _collisionShape.Position = new Vector2(_collisionShape.Position.X, isLowProfile ? -15.5f : -20.0f);
 
-        string animation = "Idle";
-
-        if (crouching && !jumping && !falling || rolling)
-        {
-            animation = walking ? "Crouch_Walk" : "Crouch";
-            if (animation == walking)
-                animation = "Roll";
-        }
-        else
-        {
-            if (jumping)
-                animation = "Jump";
-            else if (falling)
-                animation = "Fall";
-            else if (walking)
-                animation = "Run";
-            else
-                animation = "Idle";
-        }
+        string animation = _isRolling ? "Roll" :
+            crouching && !jumping && !falling ? (walking ? "Crouch_Walk" : "Crouch") :
+            jumping ? "Jump" :
+            falling ? "Fall" :
+            walking ? "Run" : "Idle";
 
         if (_animations.Animation != animation)
-        {
             _animations.Play(animation);
-        }
 
-        if (Math.Abs(velX) > 0.1f)
-        {
+        if (walking && !_isRolling)
             _animations.FlipH = velX < 0;
-        }
     }
 }
-
